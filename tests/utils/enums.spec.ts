70fd68458b2252078705300ccdf17782338cad48
Chris Pikul <chris@workmaple.com>
2021-08-06 08:09:48 -0700

    tests for utils/enums

---
+++ tests/utils/enums.spec.ts
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+import { describe } from 'mocha';
+
+import { enumHas } from '../../src/utils/enums';
+
+enum ETest {
+  SOME,
+  GOOD,
+  VALUES,
+};
+
+enum ETestStr {
+  SOME = "SOME",
+  GOOD = "GOOD",
+  VALUES = "VALUES",
+};
+
+describe('Utility: enumHas()', () => {
+  it('returns true for valid TS enum', () => {
+    expect(enumHas(ETest, 'GOOD')).to.be.true;
+  });
+
+  it('returns false for invalid TS enum', () => {
+    expect(enumHas(ETest, 'BAD')).to.be.false;
+  });
+});
