683bdb12b3ec13e63de2fc3d27f6f5c319177a81
Chris Pikul <chris@workmaple.com>
2023-03-30 07:43:09 -0700

    move away from ts enum: die sizes

--- src/die-size.ts
+++ src/die-size.ts
@@ -10,17 +10,18 @@ import { enumHas } from './utils/enums';
  * The "UNKNOWN" enum maps to 0 for null values and should
  * not be treated as valid.
  */
-export enum DieSize {
-    UNKNOWN = 'UNKNOWN',
+export const DieSizes = {
+  UNKNOWN: 'UNKNOWN',
 
-    D2 = 'D2',
-    D4 = 'D4',
-    D6 = 'D6',
-    D8 = 'D8',
-    D10 = 'D10',
-    D12 = 'D12',
-    D20 = 'D20',
-    D100= 'D100',
-};
+  D2: 'D2',
+  D4: 'D4',
+  D6: 'D6',
+  D8: 'D8',
+  D10: 'D10',
+  D12: 'D12',
+  D20: 'D20',
+  D100: 'D100',
+} as const;
+export type EDieSize = typeof DieSizes[keyof typeof DieSizes];
 
-export const dieSizeHas = (key:string):boolean => enumHas(DieSize, key);
+export const dieSizeHas = (key:string):boolean => enumHas(DieSizes, key);
