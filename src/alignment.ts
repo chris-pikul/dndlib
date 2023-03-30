7761670d85c9cc1bc168f36b57ab3feb37ebc12e
Chris Pikul <chris@workmaple.com>
2023-03-30 06:32:28 -0700

    moving away from ts enums: alignments

--- src/alignment.ts
+++ src/alignment.ts
@@ -2,7 +2,7 @@
  * Copyright(C) 2021 Chris Pikul. Under MIT license. 
  * See "LICENSE" in the root project folder.
  */
-import { enumHas, isPlainObject } from './utils';
+import { enumHas, isPlainObject, StringEnum } from './utils';
 
 /**
  * Holds enumerated values for alignment
@@ -10,75 +10,69 @@ import { enumHas, isPlainObject } from './utils';
  * The "UNKNOWN" enum maps to 0 for null values and should
  * not be treated as valid.
  * 
- * This maps to the scema "/alignment.schema.json"
+ * This maps to the scema "/Alignments.schema.json"
  */
-export enum Alignment {
-    UNKNOWN = 'UNKNOWN',
+export const Alignments = {
+  UNKNOWN: 'UNKNOWN',
+  CHAOTIC_GOOD: 'CHAOTIC_GOOD',
+  GOOD: 'GOOD',
+  LAWFUL_GOOD: 'LAWFUL_GOOD',
+  CHAOTIC_NEUTRAL: 'CHAOTIC_NEUTRAL',
+  NEUTRAL: 'NEUTRAL',
+  LAWFUL_NEUTRAL: 'LAWFUL_NEUTRAL',
+  CHAOTIC_EVIL: 'CHAOTIC_EVIL',
+  EVIL: 'EVIL',
+  LAWFUL_EVIL: 'LAWFUL_EVIL',
+} as const;
+export type EAlignment = typeof Alignments[keyof typeof Alignments];
 
-    CHAOTIC_GOOD = 'CHAOTIC_GOOD',
-
-    GOOD = 'GOOD',
-
-    LAWFUL_GOOD = 'LAWFUL_GOOD',
-
-    CHAOTIC_NEUTRAL = 'CHAOTIC_NEUTRAL',
-
-    NEUTRAL = 'NEUTRAL',
-
-    LAWFUL_NEUTRAL = 'LAWFUL_NEUTRAL',
-
-    CHAOTIC_EVIL = 'CHAOTIC_EVIL',
-
-    EVIL = 'EVIL',
-
-    LAWFUL_EVIL = 'LAWFUL_EVIL',
-};
-
-export const alignmentHas = (key:string):boolean => enumHas(Alignment, key);
+export const alignmentHas = (key:string):boolean => enumHas(Alignments, key);
 
 /**
- * The entropy axis of the alignment.
+ * The entropy axis of the Alignments.
  * Can be either chaotic, neutral, or lawful.
  */
-export enum AlignmentEntropy {
-  CHAOTIC = 'CHAOTIC',
-  NEUTRAL = 'NEUTRAL',
-  LAWFUL = 'LAWFUL',
-};
+export const AlignmentEntropies = {
+  CHAOTIC: 'CHAOTIC',
+  NEUTRAL: 'NEUTRAL',
+  LAWFUL: 'LAWFUL',
+} as const;
+export type EAlignmentEntropy = typeof AlignmentEntropies[keyof typeof AlignmentEntropies];
 
-export const alignmentEntropyHas = (key:string):boolean => enumHas(AlignmentEntropy, key);
+export const alignmentEntropyHas = (key:string):boolean => enumHas(AlignmentEntropies, key);
 
 /**
- * The morality axis of the alignment.
+ * The morality axis of the Alignments.
  * Can be either good, neutral, or evil.
  */
-export enum AlignmentMorality {
-  GOOD = 'GOOD',
-  NEUTRAL = 'NEUTRAL',
-  EVIL = 'EVIL',
-};
+export const AlignmentMoralities = {
+  GOOD: 'GOOD',
+  NEUTRAL: 'NEUTRAL',
+  EVIL: 'EVIL',
+} as const;
+export type EAlignmentMorality = typeof AlignmentMoralities[keyof typeof AlignmentMoralities];
 
-export const alignmentMoralityHas = (key:string):boolean => enumHas(AlignmentMorality, key);
+export const alignmentMoralityHas = (key:string):boolean => enumHas(AlignmentMoralities, key);
 
 /**
  * Holds the two axes of alignment as one value.
  * These are entropy, and morality.
  */
 export interface IAlignmentAxes {
-  entropy : AlignmentEntropy;
-  morality : AlignmentMorality;
+  entropy : EAlignmentEntropy;
+  morality : EAlignmentMorality;
 };
 
 /**
- * An alternative class for holding alignment.
+ * An alternative class for holding Alignments.
  * Instead of singular enums, this can be used to break apart the axes used.
  * 
  * Allows easier testing and equating as well.
  */
 export class AlignmentAxes implements IAlignmentAxes {
-  entropy : AlignmentEntropy = AlignmentEntropy.NEUTRAL;
+  entropy : EAlignmentEntropy = AlignmentEntropies.NEUTRAL;
 
-  morality : AlignmentMorality = AlignmentMorality.NEUTRAL;
+  morality : EAlignmentMorality = AlignmentMoralities.NEUTRAL;
 
   constructor(props?:any) {
     if(typeof props !== 'undefined' && props !== null) {
@@ -110,63 +104,63 @@ export class AlignmentAxes implements IAlignmentAxes {
    * @param value Alignment or string enum
    * @returns TypeError if the enum is invalid
    */
-  assignFromEnum = (value : Alignment|string):(TypeError|undefined) => {
+  assignFromEnum = (value : EAlignment|string):(TypeError|undefined) => {
     switch(value) {
-      case Alignment.CHAOTIC_GOOD:
-        this.entropy = AlignmentEntropy.CHAOTIC;
-        this.morality = AlignmentMorality.GOOD;
+      case Alignments.CHAOTIC_GOOD:
+        this.entropy = AlignmentEntropies.CHAOTIC;
+        this.morality = AlignmentMoralities.GOOD;
         break;
-      case Alignment.GOOD:
-        this.entropy = AlignmentEntropy.NEUTRAL;
-        this.morality = AlignmentMorality.GOOD;
+      case Alignments.GOOD:
+        this.entropy = AlignmentEntropies.NEUTRAL;
+        this.morality = AlignmentMoralities.GOOD;
         break;
-      case Alignment.LAWFUL_GOOD:
-        this.entropy = AlignmentEntropy.CHAOTIC;
-        this.morality = AlignmentMorality.GOOD;
+      case Alignments.LAWFUL_GOOD:
+        this.entropy = AlignmentEntropies.CHAOTIC;
+        this.morality = AlignmentMoralities.GOOD;
         break;
-      case Alignment.CHAOTIC_NEUTRAL:
-        this.entropy = AlignmentEntropy.CHAOTIC;
-        this.morality = AlignmentMorality.NEUTRAL;
+      case Alignments.CHAOTIC_NEUTRAL:
+        this.entropy = AlignmentEntropies.CHAOTIC;
+        this.morality = AlignmentMoralities.NEUTRAL;
         break;
-      case Alignment.NEUTRAL:
-        this.entropy = AlignmentEntropy.NEUTRAL;
-        this.morality = AlignmentMorality.NEUTRAL;
+      case Alignments.NEUTRAL:
+        this.entropy = AlignmentEntropies.NEUTRAL;
+        this.morality = AlignmentMoralities.NEUTRAL;
         break;
-      case Alignment.LAWFUL_NEUTRAL:
-        this.entropy = AlignmentEntropy.CHAOTIC;
-        this.morality = AlignmentMorality.NEUTRAL;
+      case Alignments.LAWFUL_NEUTRAL:
+        this.entropy = AlignmentEntropies.CHAOTIC;
+        this.morality = AlignmentMoralities.NEUTRAL;
         break;
-      case Alignment.CHAOTIC_EVIL:
-        this.entropy = AlignmentEntropy.CHAOTIC;
-        this.morality = AlignmentMorality.EVIL;
+      case Alignments.CHAOTIC_EVIL:
+        this.entropy = AlignmentEntropies.CHAOTIC;
+        this.morality = AlignmentMoralities.EVIL;
         break;
-      case Alignment.EVIL:
-        this.entropy = AlignmentEntropy.NEUTRAL;
-        this.morality = AlignmentMorality.EVIL;
+      case Alignments.EVIL:
+        this.entropy = AlignmentEntropies.NEUTRAL;
+        this.morality = AlignmentMoralities.EVIL;
         break;
-      case Alignment.LAWFUL_EVIL:
-        this.entropy = AlignmentEntropy.CHAOTIC;
-        this.morality = AlignmentMorality.EVIL;
+      case Alignments.LAWFUL_EVIL:
+        this.entropy = AlignmentEntropies.CHAOTIC;
+        this.morality = AlignmentMoralities.EVIL;
         break;
       default:
-        return new TypeError(`The alignment enumerated value "${value}" does not map to a valid Alignment.`);
+        return new TypeError(`The alignment enumerated value "${value}" does not map to a valid Alignments.`);
     }
   }
 
   get isChaotic():boolean {
-    return this.entropy === AlignmentEntropy.CHAOTIC;
+    return this.entropy === AlignmentEntropies.CHAOTIC;
   }
 
   get isLawful():boolean {
-    return this.entropy === AlignmentEntropy.LAWFUL;
+    return this.entropy === AlignmentEntropies.LAWFUL;
   }
 
   get isGood():boolean {
-    return this.morality === AlignmentMorality.GOOD;
+    return this.morality === AlignmentMoralities.GOOD;
   }
 
   get isEvil():boolean {
-    return this.morality === AlignmentMorality.EVIL;
+    return this.morality === AlignmentMoralities.EVIL;
   }
 
   /**
@@ -174,28 +168,28 @@ export class AlignmentAxes implements IAlignmentAxes {
    * 
    * @returns Alignment enum, or undefined
    */
-  toEnum = ():(Alignment|undefined) => {
-    if(this.entropy === AlignmentEntropy.CHAOTIC) {
-      if(this.morality === AlignmentMorality.GOOD)
-        return Alignment.CHAOTIC_GOOD;
-      else if(this.morality === AlignmentMorality.NEUTRAL)
-        return Alignment.CHAOTIC_NEUTRAL;
-      else if(this.morality === AlignmentMorality.EVIL)
-        return Alignment.CHAOTIC_EVIL;
-    } else if(this.entropy === AlignmentEntropy.NEUTRAL) {
-      if(this.morality === AlignmentMorality.GOOD)
-        return Alignment.GOOD;
-      else if(this.morality === AlignmentMorality.NEUTRAL)
-        return Alignment.NEUTRAL;
-      else if(this.morality === AlignmentMorality.EVIL)
-        return Alignment.EVIL;
-    } else if(this.entropy === AlignmentEntropy.LAWFUL) {
-      if(this.morality === AlignmentMorality.GOOD)
-        return Alignment.LAWFUL_GOOD;
-      else if(this.morality === AlignmentMorality.NEUTRAL)
-        return Alignment.LAWFUL_NEUTRAL;
-      else if(this.morality === AlignmentMorality.EVIL)
-        return Alignment.LAWFUL_EVIL;
+  toEnum = ():(EAlignment|undefined) => {
+    if(this.entropy === AlignmentEntropies.CHAOTIC) {
+      if(this.morality === AlignmentMoralities.GOOD)
+        return Alignments.CHAOTIC_GOOD;
+      else if(this.morality === AlignmentMoralities.NEUTRAL)
+        return Alignments.CHAOTIC_NEUTRAL;
+      else if(this.morality === AlignmentMoralities.EVIL)
+        return Alignments.CHAOTIC_EVIL;
+    } else if(this.entropy === AlignmentEntropies.NEUTRAL) {
+      if(this.morality === AlignmentMoralities.GOOD)
+        return Alignments.GOOD;
+      else if(this.morality === AlignmentMoralities.NEUTRAL)
+        return Alignments.NEUTRAL;
+      else if(this.morality === AlignmentMoralities.EVIL)
+        return Alignments.EVIL;
+    } else if(this.entropy === AlignmentEntropies.LAWFUL) {
+      if(this.morality === AlignmentMoralities.GOOD)
+        return Alignments.LAWFUL_GOOD;
+      else if(this.morality === AlignmentMoralities.NEUTRAL)
+        return Alignments.LAWFUL_NEUTRAL;
+      else if(this.morality === AlignmentMoralities.EVIL)
+        return Alignments.LAWFUL_EVIL;
     }
   }
 
