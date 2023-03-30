11be6c57ab7552beda0fd1eb36b2feeaf98d76ac
Chris Pikul <chris@workmaple.com>
2023-03-30 07:47:56 -0700

    move away from ts enum: resource type

--- src/resource-type.ts
+++ src/resource-type.ts
@@ -10,36 +10,37 @@ import { enumHas } from './utils/enums';
  * 
  * Schema: /resource-type.schema.json
  */
-export enum ResourceType {
-    UNKNOWN = 'UNKNOWN',
-    ANY = 'ANY',
+export const ResourceTypes = {
+  UNKNOWN: 'UNKNOWN',
+  ANY: 'ANY',
 
-    ABILITY_SCORE = 'ABILITY_SCORE',
-    ACTION = 'ACTION',
-    BACKGROUND = 'BACKGROUND',
-    CLASS = 'CLASS',
-    CLASS_FEATURE = 'CLASS_FEATURE',
-    CONDITION = 'CONDITION',
-    DAMAGE_TYPE = 'DAMAGE_TYPE',
-    DEITY = 'DEITY',
-    DISEASE = 'DISEASE',
-    EQUIPMENT_PACK = 'EQUIPMENT_PACK',
-    FEAT = 'FEAT',
-    HAZARD = 'HAZARD',
-    ITEM = 'ITEM',
-    ITEM_CATEGORY = 'ITEM_CATEGORY',
-    LANGUAGE = 'LANGUAGE',
-    MAGIC_SCHOOL = 'MAGIC_SCHOOL',
-    MONSTER = 'MONSTER',
-    PROFICIENCY = 'PROFICIENCY',
-    RACE = 'RACE',
-    SKILL = 'SKILL',
-    SPELL = 'SPELL',
-    SUB_RACE = 'SUB_RACE',
-    TRAIT = 'TRAIT',
-    TRAP = 'TRAP',
-    VEHICLE = 'VEHICLE',
-    WEAPON_PROPERTY = 'WEAPON_PROPERTY',
-};
+  ABILITY_SCORE: 'ABILITY_SCORE',
+  ACTION: 'ACTION',
+  BACKGROUND: 'BACKGROUND',
+  CLASS: 'CLASS',
+  CLASS_FEATURE: 'CLASS_FEATURE',
+  CONDITION: 'CONDITION',
+  DAMAGE_TYPE: 'DAMAGE_TYPE',
+  DEITY: 'DEITY',
+  DISEASE: 'DISEASE',
+  EQUIPMENT_PACK: 'EQUIPMENT_PACK',
+  FEAT: 'FEAT',
+  HAZARD: 'HAZARD',
+  ITEM: 'ITEM',
+  ITEM_CATEGORY: 'ITEM_CATEGORY',
+  LANGUAGE: 'LANGUAGE',
+  MAGIC_SCHOOL: 'MAGIC_SCHOOL',
+  MONSTER: 'MONSTER',
+  PROFICIENCY: 'PROFICIENCY',
+  RACE: 'RACE',
+  SKILL: 'SKILL',
+  SPELL: 'SPELL',
+  SUB_RACE: 'SUB_RACE',
+  TRAIT: 'TRAIT',
+  TRAP: 'TRAP',
+  VEHICLE: 'VEHICLE',
+  WEAPON_PROPERTY: 'WEAPON_PROPERTY',
+} as const;
+export type EResourceType = typeof ResourceTypes[keyof typeof ResourceTypes];
 
-export const resourceTypeHas = (key:string):boolean => enumHas(ResourceType, key);
+export const resourceTypeHas = (key:string):boolean => enumHas(ResourceTypes, key);
