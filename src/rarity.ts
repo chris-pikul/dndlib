683bfc6c81637fc22282e15af69b46c01cc2e950
Chris Pikul <chris@workmaple.com>
2023-03-30 07:45:58 -0700

    move away from ts enum: rarity

--- src/rarity.ts
+++ src/rarity.ts
@@ -3,13 +3,14 @@ import { enumHas } from './utils';
 /**
  * Describes the rarity of an item.
  */
-export enum Rarity {
-  COMMON = 'COMMON',
-  UNCOMMON = 'UNCOMMON',
-  RARE = 'RARE',
-  VERY_RARE = 'VERY_RARE',
-  LEGENDARY = 'LEGENDARY',
-  ARTIFACT = 'ARTIFACT',
-};
+export const Rarities = {
+  COMMON: 'COMMON',
+  UNCOMMON: 'UNCOMMON',
+  RARE: 'RARE',
+  VERY_RARE: 'VERY_RARE',
+  LEGENDARY: 'LEGENDARY',
+  ARTIFACT: 'ARTIFACT',
+} as const;
+export type ERarity = typeof Rarities[keyof typeof Rarities];
 
-export const rarityHas = (key:string):boolean => enumHas(Rarity, key);
+export const rarityHas = (key:string):boolean => enumHas(Rarities, key);
