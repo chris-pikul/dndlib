d7e5262b4cbe97ce75ab560690836f7ef8545f54
Chris Pikul <chris@workmaple.com>
2023-03-30 07:50:40 -0700

    move away from ts enum: shape

--- src/shape.ts
+++ src/shape.ts
@@ -4,12 +4,12 @@ import { enumHas } from './utils';
  * Describes a shape.
  * Commonly used for area-of-effects.
  */
-export enum Shape {
-  LINE = 'LINE',
-  CUBE = 'CUBE',
-  CONE = 'CONE',
-  CYLINDER = 'CYLINDER',
-  SPHERE = 'SPHERE',
-}
+export const Shapes = {
+  LINE: 'LINE',
+  CUBE: 'CUBE',
+  CONE: 'CONE',
+  CYLINDER: 'CYLINDER',
+  SPHERE: 'SPHERE',
+} as const;
 
-export const shapeHas = (key:string):boolean => enumHas(Shape, key);
+export const shapeHas = (key:string):boolean => enumHas(Shapes, key);
